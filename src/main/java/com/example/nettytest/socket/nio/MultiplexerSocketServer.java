cd5ff3e1a14ae8c2c1de118951493ab72318de37
liujianwei <499356212@qq.com>
2019-09-24 10:59:31 +0800

    '添加nio并发读写次数打印'

--- src/main/java/com/example/nettytest/socket/nio/MultiplexerSocketServer.java
+++ src/main/java/com/example/nettytest/socket/nio/MultiplexerSocketServer.java
@@ -23,6 +23,8 @@ public class MultiplexerSocketServer implements Runnable {
 
     private volatile boolean stop;
 
+    private int count;
+
 
     public MultiplexerSocketServer(int port) {
         try {
@@ -36,10 +38,6 @@ public class MultiplexerSocketServer implements Runnable {
             e.printStackTrace();
             System.exit(1);
         }
-    }
-
-    public void stop() {
-        this.stop = true;
     }
 
     @Override
@@ -56,9 +54,9 @@ public class MultiplexerSocketServer implements Runnable {
                     try {
                         handleInput(key);
                     } catch (Exception e) {
-                        if(key != null){
+                        if (key != null) {
                             key.cancel();
-                            if(key.channel() != null){
+                            if (key.channel() != null) {
                                 key.channel().close();
                             }
                         }
@@ -69,7 +67,7 @@ public class MultiplexerSocketServer implements Runnable {
             }
         }
         // 多路复用器关闭后，所有注册在上面的Channel和Pipe等资源都会被自动去除注册并关闭，所以不需要重复释放资源
-        if(selector != null){
+        if (selector != null) {
             try {
                 selector.close();
             } catch (Exception e) {
@@ -79,41 +77,44 @@ public class MultiplexerSocketServer implements Runnable {
     }
 
     private void handleInput(SelectionKey key) throws Exception {
-        if (key.isValid()) {
-            if (key.isAcceptable()) {
-                // 接受新的连接
-                ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
-                SocketChannel socketChannel = serverSocketChannel.accept();
-                socketChannel.configureBlocking(false);
-                // 将新的连接注册到selector上，
-                socketChannel.register(selector, SelectionKey.OP_READ);
-            }
-            if (key.isReadable()) {
-                SocketChannel socketChannel = (SocketChannel) key.channel();
-                ByteBuffer readBuffer = ByteBuffer.allocate(1024);
-                int readBytes = socketChannel.read(readBuffer);
-                if (readBytes > 0) {
-                    readBuffer.flip();
-                    byte[] bytes = new byte[readBuffer.remaining()];
-                    readBuffer.get(bytes);
-                    String body = new String(bytes, "UTF-8");
-                    System.out.println("Server receive message :" + body);
+        synchronized (this) {
+            if (key.isValid()) {
+                if (key.isAcceptable()) {
+                    // 接受新的连接
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    socketChannel.configureBlocking(false);
+                    // 将新的连接注册到selector上，
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                }
+                if (key.isReadable()) {
+                    count++;
+                    System.out.println(count);
+                    SocketChannel socketChannel = (SocketChannel) key.channel();
+                    ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                    int readBytes = socketChannel.read(readBuffer);
+                    if (readBytes > 0) {
+                        readBuffer.flip();
+                        byte[] bytes = new byte[readBuffer.remaining()];
+                        readBuffer.get(bytes);
+                        String body = new String(bytes, "UTF-8");
+                        System.out.println("Server receive message :" + body);
 
-                    String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date().toString() : "BAD QUERY";
-                    doWrite(socketChannel, currentTime);
-                } else if (readBytes < 0) {
-                    key.cancel();
-                    socketChannel.close();
-                } else {
-                    // 读取到0字节忽略
+                        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date().toString() : "BAD QUERY";
+                        doWrite(socketChannel, currentTime);
+                    } else if (readBytes < 0) {
+                        key.cancel();
+                        socketChannel.close();
+                    } else {
+                        // 读取到0字节忽略
+                    }
                 }
             }
         }
-
     }
 
     private void doWrite(SocketChannel channel, String response) throws Exception {
-        if(response != null || response.trim().length() > 0){
+        if (response != null || response.trim().length() > 0) {
             System.out.println("Server answer message : " + response);
             byte[] bytes = response.getBytes();
             ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
